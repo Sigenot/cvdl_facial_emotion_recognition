87cc8381d9bbbb786e6b81c43037a43a00cd8ed1
Sigenot <tobias.kreil@proton.me>
2025-07-16 12:32:48 +0200

    importing libraries

--- fer_cvdl.py
+++ fer_cvdl.py
@@ -1,2 +1,10 @@
-import cv2
-from fer import FER
+from torchvision import datasets, transforms
+import torch
+import torch.nn as nn
+import numpy as np
+from matplotlib import pyplot as plt
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import deeplake
+from tqdm import tqdm
+
